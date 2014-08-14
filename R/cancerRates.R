2ae882250dc4cdd791a77ab77634b81854b8a073
Patrick Brown <patrick.brown@utoronto.ca>
2014-08-14 22:23:09 +0000

    version 0.7.7

--- R/cancerRates.R
+++ R/cancerRates.R
@@ -33,29 +33,34 @@ Iowa=8400899,
 "New Mexico"=8401399
 )
 if(is.character(area)) {
-area = areaCodes[grep(paste("^", area[1], sep=""), names(areaCodes), ignore.case=TRUE)]
+area = areaCodes[grep(paste("^", area[1], sep=""), 
+				names(areaCodes), ignore.case=TRUE)]
 } 
 
 
 result = list()
 
-  rates=NULL
-  for(Dsex in names(sexes)) {
-fs<-paste("http://ci5.iarc.fr/CI5plus/Table4r.asp?registry=",area,(paste("&period=",year,sep="",collapse="")),"&sex=", sexes[Dsex],"&window=1&text=1&stat=0&submit=Execute",sep="")
-tempn = scan(fs, what="a", quiet=T)
-theurl=(paste("http://ci5.iarc.fr/", gsub("^HREF=", "", grep("href=/data", iconv(tempn,to="UTF-8"), value=T, ignore.case=T)), sep=""))
-result[[Dsex]]=read.table(url(theurl), header=T,skip=1, fill=T, sep="\t", as.is=T)
-forAttribute = scan(url(theurl), what="a", sep="\t", nmax=1, quiet=T)
+rates=NULL
+for(Dsex in names(sexes)) {
+fs<-paste("http://ci5.iarc.fr/CI5plus/old/Table4r.asp?registry=",area,(paste("&period=",year,sep="",collapse="")),"&sex=", sexes[Dsex],"&window=1&text=1&stat=0&submit=Execute",sep="")
+tempn = scan(fs, what="a", quiet=TRUE)
+theurl=(paste("http://ci5.iarc.fr/", 
+					gsub("^HREF=", "", grep("href=/data", 
+									iconv(tempn,to="UTF-8"), value=TRUE, 
+									ignore.case=TRUE)), sep=""))
+theurl = url(theurl)
+forAttribute = scan(theurl, what="a", sep="\t", nmax=1, quiet=TRUE)
+result[[Dsex]]=read.table(theurl, header=TRUE,skip=1, 
+		fill=TRUE, sep="\t", as.is=TRUE)
+}
 
-  }
-
-
+iarcSite=NULL
 for (Dsex in names(sexes)) {
  cancerTypes =  result[[Dsex]][,1]
   
  cancerTypes = gsub("[[:space:]]+$", "", cancerTypes)
 
-siterow = grep(site, cancerTypes,ignore.case=T)
+siterow = grep(site, cancerTypes,ignore.case=TRUE)
 if(length(siterow) > 1 )    {
   warning(paste("matched ", paste(cancerTypes[siterow], collapse=","), "\n from cancer types", paste(cancerTypes, collapse=","), 
   "\n using", cancerTypes[siterow[1]]))
@@ -64,7 +69,8 @@ if(length(siterow) > 1 )    {
 if(length(siterow) ) {
   x=result[[Dsex]][siterow,]
 iarcSite =gsub("[[:space:]]+$", "", x[1]) 
-  x = x[,-c(1,grep("Total|Unk|ICD|CR|ASR", names(x), ignore.case=T))]
+  x = x[,-c(1,grep("Total|Unk|ICD|CR|ASR", 
+						  names(x), ignore.case=TRUE))]
   x = as.vector(x)
   names(x) = gsub("^X|\\.$", "", names(x))
   rates[[Dsex]]=x
@@ -76,7 +82,7 @@ names(rates) = gsub("\\.", "_", names(rates))
 attributes(rates)$site = iarcSite
 forAttribute = strsplit(forAttribute, "\\(")[[1]]
 attributes(rates)$area = gsub("[[:space:]]+$", "", forAttribute[1])
-attributes(rates)$year = gsub("\\)$", "", forAttribute[2])
+attributes(rates)$year = gsub("\\)$", "", forAttribute[length(forAttribute)])
 
 return(rates)
 
